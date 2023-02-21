fa2de0c47ff1ed39328410b6a0335acbb3e205cd
nikolayilin <n.ilin@it-park.tech>
2023-02-21 15:20:22 +0300

    new_3

--- app/implemented.py
+++ app/implemented.py
@@ -4,7 +4,7 @@ from app.dao.movie import MovieDAO
 from app.service.director import DirectorService
 from app.service.genre import GenreService
 from app.service.movie import MovieService
-from setup_db import db
+from app.setup_db import db
 
 movie_dao = MovieDAO(db.session)
 movie_service = MovieService(movie_dao)
